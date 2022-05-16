d3fcd288b0a3831b6d0467ee4cea91ead4e567aa
Sunitha101199 <sunitha101199@msitprogram.net>
2022-05-17 00:54:07 +0530

    Add heroku settings to production

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -11,3 +11,34 @@ ALLOWED_HOSTS = [
     'sunitha-basic-blog.herokuapp.com',
     'sunitha.blog',
 ]
+
+DEBUG_PROPAGATE_EXCETIONS = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.Streamandler',
+        },
+    },
+    'loggers': {
+        'MYAPP': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
+#Heroku Settings
+django_on_heroku.settings(locals(), staticfiles=False)
+del DATABASES['default']['OPTIONS']['sslmode']
