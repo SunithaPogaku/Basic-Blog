74cf50d004547e79ad8ccd9c355c26194ef8b9d0
Sunitha101199 <sunitha101199@msitprogram.net>
2022-05-17 00:20:36 +0530

    Initial commit

---
+++ config/settings/prod.py
@@ -0,0 +1,13 @@
+import django_on_heroku
+from decouple import config
+
+from .base import *
+
+SECRET_KEY = config('SECRET_KEY')
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    'sunitha-basic-blog.herokuapp.com',
+    'sunitha.blog',
+]
